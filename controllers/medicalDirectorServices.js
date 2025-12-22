77c78aa1409ede2bb0ca09156046831cc9980b99
vishalsainiICA <vishal.saini@icaimpetus.com>
2025-12-22 18:23:53 +0530

    add medical director dashboard

--- controllers/medicalDirectorServices.js
+++ controllers/medicalDirectorServices.js
@@ -2,7 +2,8 @@ import bcrypt from "bcryptjs";
 import { generateToken } from "../utills/jwtToken.js";
 import UserModel from "../models/userModel.js";
 import HospitalModel from "../models/hospital.js";
-import DepartmentModel from "../models/departmentModel.js";
+import PatientModel from "../models/patientModel.js"
+
 
 export const getProfile = async (req, res) => {
     console.log(req.user);
@@ -24,9 +25,6 @@ export const findHospitalByMedicalDirectorId = async (req, res) => {
         const user = req.user
         if (!user.id) return res.status(400).json({ message: 'medicalDirector id is requried' })
 
-        console.log(user);
-
-
         const hosptial = await HospitalModel.findOne({ medicalDirector: user.id, isDeleted: false })
         return res.status(200).json({ message: 'success', data: hosptial })
 
@@ -37,21 +35,22 @@ export const findHospitalByMedicalDirectorId = async (req, res) => {
     }
 }
 
-export const AllDepartmentsByHospitalById = async (req, res) => {
+export const hosptialPatients = async (req, res) => {
     try {
         const user = req.user
-        const profile = await UserModel.findById(user?.id)
+        const profile = await UserModel.findById(user?.id).populate('hospitalId')
         if (!profile) return res.status(404).json({ message: "user not found" });
 
-        console.log(profile);
+        const patients = await PatientModel.find({ isDeleted: false, hospitalId: profile?.hospitalId, prescribtionId: { $ne: null } }).populate('hospitalId doctorId prescribtionId initialAssementId')
 
-
-        const departments = await DepartmentModel.find({ hospitalId: profile.hospitalId, isDeleted: false }).populate('doctorIds')
-        return res.status(200).json({ message: 'success', data: departments })
+        return res.status(200).json({
+            message: "success",
+            status: 200,
+            data: patients
+        });
 
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Internal Server Error' })
-
+        console.error("Error:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
 }
--- routes/medicalDirectorRoutes.js
+++ routes/medicalDirectorRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utills/jwtToken.js";
+import { findHospitalByMedicalDirectorId, getProfile, hosptialPatients } from "../controllers/medicalDirectorServices.js";
 
 
 
 const app = express.Router()
 
-// app.post('create-hosptial', createHospital)
+app.get('/auth/profile', verifyToken, getProfile)
+app.get('/getHospital', verifyToken, findHospitalByMedicalDirectorId)  
+app.get('/patients/hospitalAllPaitent', verifyToken, hosptialPatients)
 // app.post('create-medicalDirector', createHospital)
 // app.get('/auth/profile', verifyToken, getProfile)
 
